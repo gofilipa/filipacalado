ca668c5dfb8032258444448bf48d143103fe0337
Filipa Calado <gofilipa@gmail.com>
2023-03-06 21:18:33 -0500

    first commit

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+application = app
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/qt')
+def qt():
+    return render_template('qt.html')
+
+@app.route('/teach')
+def teach():
+    return render_template('teach.html')
+
+@app.route('/cv')
+def cv():
+    return render_template('cv.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
